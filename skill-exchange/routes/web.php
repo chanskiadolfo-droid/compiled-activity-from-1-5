0f4d8cf89545ec9e28de2f5929cdad5ada3bd70c
Christian A. Dampil <chanskiadolfo@gmail.com>
2026-05-14 17:27:30 +0800

    Clean repo: keep only Activity 1-3

--- skill-exchange/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SkillController;
-
-Route::get('/', fn() => redirect('/skills'));
-Route::get('/skills', [SkillController::class, 'index']);
-Route::get('/skills/{id}', [SkillController::class, 'show']);
